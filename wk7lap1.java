16be330bc0cabf41ffe2e59a37bcf519f6e9db0b
linkinstar <linkinstar22@gmail.com>
2015-03-24 20:54:01 -0700

    Update wk7lap1.java

--- wk7lap1.java
+++ wk7lap1.java
@@ -1,52 +1,72 @@
-package test;
-import java.util.Random;
-import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class test {
-public static void main(String [] args)
-{
-	Random rand = new Random();
-	Scanner input=new Scanner(System.in);
-	String choice;
-	int playerturn =0;
-	int turn=1;
-	System.out.println("Welcome to rock-paper-scissor game!!");
-	// 1=rock,2=Paper,3=Scissor
-	while(turn !=0)
-	{
-		int computerturn = rand.nextInt(3);
-		System.out.println("Enter your choice (Rock,Paper,Scissor:)");
-		choice = input.next();
-		switch ( choice.toLowerCase())
-		{
-		case "rock":
-		playerturn =1;
-		break;
-		case "paper":
-			playerturn =2;
-			break;
-		case "scissor":
-			playerturn =3;
-			break;
+	public static void main(String[] args) {
+		int computerturn;
+		String choice;
+		String playturn;
+		int turn = 1;
+		while (turn != 0) {
+			computerturn = 1 + (int) (Math.random() * 3);
+			choice = JOptionPane
+					.showInputDialog("Enter your choice (Rock,Paper,Scissor:)");
+			switch (choice.toLowerCase()) {
+			case "rock": {
+				switch (computerturn) {
+				case 1:
+					JOptionPane.showMessageDialog(null, "Rock tie to Rock");
+					break;
+				case 2:
+					JOptionPane.showMessageDialog(null, "Rock Lose to Paper");
+					break;
+				case 3:
+					JOptionPane.showMessageDialog(null, "Rock win to Scissor");
+				}
+			}
+				break;
+			case "paper": {
+				switch (computerturn) {
+				case 1:
+					JOptionPane.showMessageDialog(null, "Paper win to Rock");
+					break;
+				case 2:
+					JOptionPane.showMessageDialog(null, "Paper tie to Paper");
+					break;
+				case 3:
+					JOptionPane
+							.showMessageDialog(null, "Paper lose to Scissor");
+				}
+			}
+				break;
+			case "scissor": {
+				switch (computerturn) {
+				case 1:
+					JOptionPane.showMessageDialog(null, "Scissor lose to Rock");
+					break;
+				case 2:
+					JOptionPane.showMessageDialog(null, "Scissor win to Paper");
+					break;
+				case 3:
+					JOptionPane.showMessageDialog(null,
+							"Scissor tie to Scissor");
+				}
+			}
+				break;
+			default:
+				return;
+			}
+			playturn = JOptionPane
+					.showInputDialog("Do you want to play again(Y/N):");
+			switch (playturn.toUpperCase()) {
+			case "Y":
+				turn = 1;
+				break;
+			default:
+				turn = 0;
+				break;
+			}
+
 		}
-		if( computerturn ==1)
-			System.out.println("Computer play is : rock");
-		else if(computerturn ==2)
-			System.out.println("Computer play is : Paper");
-		else System.out.println("Computer play is : Scissor");
-		if(computerturn ==playerturn)
-			System.out.println("Your tie");
-			else if((computerturn ==1)&&(playerturn==3))
-				System.out.println("You Lose");
-			else if((computerturn ==2)&&(playerturn ==1))
-				System.out.println("You Lose");
-			else if ((computerturn ==3)&&(playerturn ==2))
-				System.out.println("You Lose");
-			else System.out.println("You Win");	
-		
-		System.out.println("Do you want to play again(1-Yes,0-No):");
-		turn= input.nextInt();
-		
-	}}
+	}
 }
-
-
